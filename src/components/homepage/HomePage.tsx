fa76c3300533be7dc1e4f60d714ba939fdbba503
axon4 <65050809+axon4@users.noreply.github.com>
2020-08-11 12:12:47 +0100

    create & style basic home-page

---
+++ src/components/homepage/HomePage.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './HomePage.scss';
+
+const HomePage = () => (
+	<div className='homepage'>
+		<div className='directory-menu'>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>CAT1</h1>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>CAT2</h1>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>CAT3</h1>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>CAT4</h1>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>CAT5</h1>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default HomePage;
