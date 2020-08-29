19f76f17fe53fb5303758e3d9c00830ed2651f4c
axon4 <65050809+axon4@users.noreply.github.com>
2020-08-29 16:34:53 +0100

    reMove Redux-Logger from production-build

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import rootReducer from './rootReducer';
 
-const middlewares = [logger];
+// eslint-disable-next-line
+const middlewares = new Array();
+if (process.env.NODE_ENV === 'development') {
+	middlewares.push(logger);
+};
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
