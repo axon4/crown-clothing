148fe76af540a0492a64a9f9538d421a0db384a7
axon4 <65050809+axon4@users.noreply.github.com>
2023-11-17 18:38:13 +0000

    dePloy

---
+++ precache-manifest.7bf6ece5a9027db90f28ed688428a89a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f4c6830d9d0178061e261aae1cdca4",
+    "url": "/eCommerce-store/index.html"
+  },
+  {
+    "revision": "eb8d8e234fad6bf9e445",
+    "url": "/eCommerce-store/static/css/3.d6510567.chunk.css"
+  },
+  {
+    "revision": "5fde2878c64c0f0e4d99",
+    "url": "/eCommerce-store/static/css/4.ea802436.chunk.css"
+  },
+  {
+    "revision": "8d2b5f7ee8d3664fb2b1",
+    "url": "/eCommerce-store/static/css/5.0463b0d7.chunk.css"
+  },
+  {
+    "revision": "040781e5a2300f922bdd",
+    "url": "/eCommerce-store/static/css/6.945725f6.chunk.css"
+  },
+  {
+    "revision": "bd2a61f2a95a8f041109",
+    "url": "/eCommerce-store/static/css/main.ee3cb5a8.chunk.css"
+  },
+  {
+    "revision": "945544686b1fd63e072c",
+    "url": "/eCommerce-store/static/js/2.2455e2df.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/eCommerce-store/static/js/2.2455e2df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8d8e234fad6bf9e445",
+    "url": "/eCommerce-store/static/js/3.51c03948.chunk.js"
+  },
+  {
+    "revision": "5fde2878c64c0f0e4d99",
+    "url": "/eCommerce-store/static/js/4.07f22ea0.chunk.js"
+  },
+  {
+    "revision": "8d2b5f7ee8d3664fb2b1",
+    "url": "/eCommerce-store/static/js/5.f98fd261.chunk.js"
+  },
+  {
+    "revision": "040781e5a2300f922bdd",
+    "url": "/eCommerce-store/static/js/6.e0b575e5.chunk.js"
+  },
+  {
+    "revision": "5a1d01fa4bc610916326",
+    "url": "/eCommerce-store/static/js/7.fec48b08.chunk.js"
+  },
+  {
+    "revision": "bd2a61f2a95a8f041109",
+    "url": "/eCommerce-store/static/js/main.1d39bf51.chunk.js"
+  },
+  {
+    "revision": "fa9efa4a73917be69133",
+    "url": "/eCommerce-store/static/js/runtime-main.fdf60baa.js"
+  },
+  {
+    "revision": "52b6187ddbc1581629fca9e1f07869c8",
+    "url": "/eCommerce-store/static/media/broken-mug.52b6187d.png"
+  },
+  {
+    "revision": "c778cc50d824f367d0f5b53cf5571ba4",
+    "url": "/eCommerce-store/static/media/cart.c778cc50.svg"
+  },
+  {
+    "revision": "90dbf227f8dd73bde3282bbc3a251dac",
+    "url": "/eCommerce-store/static/media/crown.90dbf227.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ecommerce-store/precache-manifest.199a0d1d49cfd5373e69e43a783a0099.js"
+  "/eCommerce-store/precache-manifest.7bf6ece5a9027db90f28ed688428a89a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ecommerce-store/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eCommerce-store/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
